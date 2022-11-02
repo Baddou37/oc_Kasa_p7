361390678a75cd37eea0d92cc4c2ffcb752a0956
Baddou37 <julien.thibault@live.fr>
2022-11-02 15:30:46 +0100

    maj

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import BGHome from "../assets/home_bg_banner.png"
 
 
 export default function Home() {
+    document.title = "Kasa - Accueil";
     return (
         <main className="display_home">
             <Banner img={BGHome} text1={'Chez vous,'} text2={'partout et ailleurs'} />
